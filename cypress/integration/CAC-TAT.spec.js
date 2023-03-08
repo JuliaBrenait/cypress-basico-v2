cc0f2d622c7c1ef7b5b09bee502b3174dbec98ff
jubsdaora@gmail.com <jubsdaora@gmail.com>
2023-03-07 21:49:23 -0300

    cy.get, type, click

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,10 +1,26 @@
 /// <reference types="Cypress" />
 
-describe('Central de Atendimento ao Cliente TAT', function() {
-    it('verifica o título da aplicação', function() {
-      cy.visit('./src/index.html');
 
-      cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
+describe('Central de Atendimento ao Cliente TAT', function () {  
+  beforeEach(() => {
+    cy.visit('./src/index.html');
+  })
+  
+  it('verifica o título da aplicação', function () {
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT');
+  })
+
+  it.only('Preenche os campos obrigatórios e envia o formulário', function () {
+    cy.get('#firstName').type('Nome')
+    cy.get('#lastName').type('Sobrenome')
+    cy.get('#email').type('Email@gmail.com')
+    cy.get('#open-text-area').type('Sugestão')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.success').should('be.visible')
+
 
     })
-  })
+
+})
+
